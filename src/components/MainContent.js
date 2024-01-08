05a3c1a3c4108be60a20d95fd7a9e26ffcef5739
TimothyMcCann <timothysmccann@gmail.com>
2024-01-08 16:31:30 -0500

    finished paired programming

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,7 +1,16 @@
 import React from "react";
+import TopMenu from "./TopMenu.js"
+import GenrePicker from "./GenrePicker.js"
+import AlbumList from "./AlbumList.js";
 
 function MainContent() {
-  return <main>{/* What components go here? */}</main>;
+  return (
+    <main>
+      <TopMenu />
+      <GenrePicker />
+      <AlbumList />
+    </main>
+  )
 }
 
 export default MainContent;
